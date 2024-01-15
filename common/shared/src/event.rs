e4680150a00745a554869e2365da8a5778f6bede
faramo_zayw <faramo_zayw@protonmail.com>
2024-01-15 17:12:38 +0200

    remove doc test

--- common/shared/src/event.rs
+++ common/shared/src/event.rs
@@ -1,19 +1,5 @@
 use soroban_sdk::{Env, IntoVal, Symbol, Val};
 
-/// # Example
-/// ```
-/// use shared::Event;
-/// use soroban_sdk::{contracttype, BytesN};
-///
-/// #[contracttype]
-/// pub struct MessageReceived {
-///     pub message: BytesN<32>,
-/// }
-///
-/// impl Event for MessageReceived {
-///     const EVENT_NAME: &'static str = "MessageReceived";
-/// }
-/// ```
 pub trait Event: IntoVal<Env, Val> + Sized {
     const EVENT_NAME: &'static str;
 
