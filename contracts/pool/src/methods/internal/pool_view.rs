5b62cb5036ac865ef9c64d7b0f83dc9a3ab811b8
faramo_zayw <faramo_zayw@protonmail.com>
2024-02-23 12:41:35 +0200

    move tests to `pool_view.rs`

--- contracts/pool/src/methods/internal/pool_view.rs
+++ contracts/pool/src/methods/internal/pool_view.rs
@@ -143,3 +143,97 @@ impl Pool {
         })
     }
 }
+
+#[allow(clippy::inconsistent_digit_grouping)]
+#[cfg(test)]
+mod tests {
+    extern crate std;
+    use std::println;
+
+    use shared::{soroban_data::SimpleSorobanData, Error};
+    use soroban_sdk::{contract, contractimpl, testutils::Address as _, Address, Env};
+
+    use crate::storage::{
+        double_values::DoubleU128,
+        pool::{Pool, Token},
+    };
+
+    #[contract]
+    pub struct TestPool;
+
+    #[contractimpl]
+    impl TestPool {
+        pub fn init(env: Env) {
+            let token_a = Address::generate(&env);
+            let token_b = Address::generate(&env);
+            Pool::from_init_params(20, token_a, token_b, (7, 7), 100, 1).save(&env);
+        }
+
+        pub fn set_balances(env: Env, new_balances: (u128, u128)) -> Result<(), Error> {
+            Pool::update(&env, |pool| {
+                pool.token_balances = DoubleU128::from(new_balances);
+                pool.total_lp_amount = pool.get_current_d();
+                Ok(())
+            })
+        }
+
+        pub fn get_receive_amount(
+            env: Env,
+            amount: u128,
+            token_from: Token,
+        ) -> Result<(u128, u128), Error> {
+            let receive_amount = Pool::get(&env)?.get_receive_amount(amount, token_from)?;
+            Ok((receive_amount.output, receive_amount.fee))
+        }
+
+        pub fn get_send_amount(
+            env: Env,
+            amount: u128,
+            token_to: Token,
+        ) -> Result<(u128, u128), Error> {
+            Pool::get(&env)?.get_send_amount(amount, token_to)
+        }
+    }
+
+    #[test]
+    fn test() {
+        let env = Env::default();
+
+        let test_pool_id = env.register_contract(None, TestPool);
+        let pool = TestPoolClient::new(&env, &test_pool_id);
+        pool.init();
+        pool.set_balances(&(200_000_000, 200_000_000));
+
+        let input = 10_000_0000000_u128;
+        let (output, fee) = pool.get_receive_amount(&input, &Token::A);
+        let (calc_input, calc_fee) = pool.get_send_amount(&output, &Token::B);
+
+        println!("input: {}", input);
+        println!("output: {}, fee: {}", output, fee);
+        println!("calc input: {}, calc fee: {}", calc_input, calc_fee);
+
+        assert_eq!(input, calc_input);
+        assert_eq!(fee, calc_fee);
+    }
+
+    #[test]
+    fn test_disbalance() {
+        let env = Env::default();
+
+        let test_pool_id = env.register_contract(None, TestPool);
+        let pool = TestPoolClient::new(&env, &test_pool_id);
+        pool.init();
+        pool.set_balances(&(200_000_000, 500_000_000));
+
+        let input = 10_000_0000000_u128;
+        let (output, fee) = pool.get_receive_amount(&input, &Token::A);
+        let (calc_input, calc_fee) = pool.get_send_amount(&output, &Token::B);
+
+        println!("input: {}", input);
+        println!("output: {}, fee: {}", output, fee);
+        println!("calc input: {}, calc fee: {}", calc_input, calc_fee);
+
+        assert_eq!(input, calc_input);
+        assert_eq!(fee, calc_fee);
+    }
+}
