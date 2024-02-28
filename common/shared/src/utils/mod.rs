4db4a610c6f3a2d0060250fcadaaea3552edcac9
faramo_zayw <faramo_zayw@protonmail.com>
2024-02-28 12:46:09 +0200

    remove unused functions

--- common/shared/src/utils/mod.rs
+++ common/shared/src/utils/mod.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{Bytes, BytesN};
+use soroban_sdk::Bytes;
 
 pub mod bytes;
 mod extend_ttl;
@@ -9,18 +9,7 @@ pub use extend_ttl::*;
 
 use crate::Error;
 
-pub fn is_bytesn32_empty(bytesn: &BytesN<32>) -> bool {
-    bytesn.is_empty() || bytesn.to_array() == [0; 32]
-}
-
 pub fn bytes_to_slice<const N: usize>(bytes: Bytes) -> [u8; N] {
-    let mut xdr_slice: [u8; N] = [0; N];
-    bytes.copy_into_slice(&mut xdr_slice);
-
-    xdr_slice
-}
-
-pub fn bytesn_to_slice<const N: usize>(bytes: BytesN<N>) -> [u8; N] {
     let mut xdr_slice: [u8; N] = [0; N];
     bytes.copy_into_slice(&mut xdr_slice);
 
@@ -32,14 +21,6 @@ pub fn merge_slices_by_half<const N: usize, const R: usize>(a: &[u8; N], b: &[u8
 
     slice[..N].copy_from_slice(a);
     slice[N..].copy_from_slice(b);
-
-    slice
-}
-
-pub fn get_slices<const N: usize>(a: &[u8; N]) -> [u8; N] {
-    let mut slice = [0u8; N];
-
-    slice[..N].copy_from_slice(a);
 
     slice
 }
